40f9bafeb61a193c74b11c82500593c62ea4d97c
ajay-2019011 <ajaysheoran2019011@gmail.com>
2024-11-24 02:33:04 +0530

    Adding nodemon and sendgrid, also logging the request

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,11 @@ connectDB();
 // Routes
 app.use("/api/contact", require("./routes/contact"));
 
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
